b28a585bc5bf2e3ec58b48313018f65eb3a376c2
abraham-yusuf <1703020028@students.unis.ac.id>
2021-09-22 19:25:03 +0000

    update 404 redirect

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,17 @@
-import styles from '../styles/globals.css';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Custom404 () {
+    const router = useRouter;
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 5000);
+    }, []);
     return(
         <div>
-            <h1 className={styles['title-not-found']}>Oppsss...</h1>
-            <h1 className={styles['title-not-found']}>Halaman Yang Anda Cari Tidak </h1>
+            <h1 className="title-not-found">Oppsss...</h1>
+            <h1 className="title-not-found">Halaman Yang Anda Cari Tidak Dapat Ditemukan.</h1>
         </div>
-    )
+    );
 }
