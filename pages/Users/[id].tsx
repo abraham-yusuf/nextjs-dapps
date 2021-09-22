be1fe56ff627526276b0924a16ee576236559f19
abraham-yusuf <1703020028@students.unis.ac.id>
2021-09-22 22:31:06 +0000

    move to dynamic id users details

--- pages/Users/[id].tsx
+++ pages/Users/[id].tsx
@@ -1,9 +1,17 @@
+import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 
 export default function UsersDetail() {
+    const router = useRouter();
+    const { id } = router.query;
     return (
         <Layout pageTitle="User Details">
             <h1>User Detail pages</h1>
+            <p>
+                {' '}
+                {id}
+                {' '}
+            </p>
         </Layout>
     );
 }
