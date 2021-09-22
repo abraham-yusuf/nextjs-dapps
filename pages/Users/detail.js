f149cbb0f9145c407ce2f16e32d423ea0eb91b22
abraham-yusuf <1703020028@students.unis.ac.id>
2021-09-22 16:33:05 +0000

    update pages

---
+++ pages/Users/detail.js
@@ -0,0 +1,7 @@
+export default function UserDetail() {
+    return (
+        <div>
+            <h1>User Detail pages</h1>
+        </div>
+    )
+}
---
+++ pages/Users/user.tsx
@@ -0,0 +1,7 @@
+export default function Users() {
+    return (
+        <div>
+            <h1>User Detail pages</h1>
+        </div>
+    )
+}
