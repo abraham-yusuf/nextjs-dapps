3c3a78f63988c8ab0b749729e22eff09dbe69cb0
abraham-yusuf <1703020028@students.unis.ac.id>
2021-09-22 21:18:48 +0000

    404 not found fix redirect

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,8 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function Custom404 () {
-    const router = useRouter;
+    const router = useRouter();
+    
     useEffect(() => {
         setTimeout(() => {
             router.push('/');
