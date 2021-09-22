68fe908022012993beb1fb514cafcdb745ebe968
abraham-yusuf <1703020028@students.unis.ac.id>
2021-09-22 18:17:47 +0000

    add Layout component and use in index

---
+++ components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import Footer from "../Footer";
+import Header from "../Header";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+export default function Layout(props: LayoutProps) {
+    const {children} = props;
+    return(
+        <div>
+            <Header />
+            <div>{children}</div>
+            <Footer />
+        </div>
+    );
+}
