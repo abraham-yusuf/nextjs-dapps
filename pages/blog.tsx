db0a86cbbb854eb291d5f652f2b9b147f30ee3ca
abraham-yusuf <1703020028@students.unis.ac.id>
2021-09-23 00:44:17 +0000

    blog card and getserverside props function

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,29 @@
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import styles from "../../styles/Users.module.css";
 
-export default function Blog() {
+export default function Blog(props) {
+    const {dataBlog} = props;
     return (
         <Layout pageTitle="Blog">
             <h1 className="title">Blog pages</h1>
+            {dataBlog.map(blog => {
+                return (
+                    <div key={blog.id} className={styles.blogcard}>
+                        <p>{blog.title}</p>
+                        <p>{blog.body}</p>
+                    </div>
+                )
+            })}
         </Layout>
     );
+}
+
+export async function getServerSideProps() {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const dataBlog = await res.json();
+    return {
+        props: {
+            dataBlog,
+        },
+    };
 }
