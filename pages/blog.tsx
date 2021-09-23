2350ebad518b6b8491d44004a13b7de771a4dc1d
abraham-yusuf <1703020028@students.unis.ac.id>
2021-09-23 00:58:40 +0000

    update blog

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,17 @@
 import Layout from "../components/Layout";
 import styles from "../styles/Blog.module.css";
 
-export default function Blog(props) {
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
+
+interface BlogProps {
+    dataBlog: Post[];
+}
+
+export default function Blog(props: BlogProps) {
     const {dataBlog} = props;
     return (
         <Layout pageTitle="Blog">
